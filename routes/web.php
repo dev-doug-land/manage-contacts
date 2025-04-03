60508cb153f1961ac98e864eff6c2d0c4e63eacf
Douglas Augusto <douglassan1@hotmail.com>
2025-04-02 21:18:48 -0300

    Feat: improvements in routes to income better readable

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/show/{contact}', [ContactController::class, 'show'])->name('contacts.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-    Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit'); 
-    Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update'); 
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+    Route::resource('contacts', ContactController::class)->except(['show', 'index']);
 
 });
 
